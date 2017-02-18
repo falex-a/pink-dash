52baea33dc69ba2ca68b1f77913afde768742487
AlexF <alexf@medaware.com>
2017-02-19 01:11:39 +0200

    added notmain

---
+++ src/main/java/main/notmain.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author alex
+ */
+public class notmain {
+    static public Integer myvar = 0;
+    notmain() {};
+}
